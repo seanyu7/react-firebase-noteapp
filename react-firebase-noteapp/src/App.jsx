7e5e6b94e522352492d165748584b3256d7885fd
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-09-14 20:45:24 +0900

    test

---
+++ react-firebase-noteapp/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Main from "./components/Main";
+import Sideber from "./components/Sideber";
+
+function App() {
+  return <div className="App">
+    <Sideber />
+    <Main />
+  </div>;
+}
+
+export default App;
---
+++ react-firebase-noteapp/src/components/Sideber.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Sideber.css"
+
+function Sideber() {
+  return (
+    <div>Sideber</div>
+  )
+}
+
+export default Sideber
