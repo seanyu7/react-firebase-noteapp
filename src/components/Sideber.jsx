a9597d052b69d71d90c4e29e66a8b5d8c5ee2a18
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-11-04 01:08:09 +0900

    made up a content of sidebar of this app

--- src/components/Sideber.jsx
+++ src/components/Sideber.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import "./Sideber.css"
+import React from "react";
+import "./Sideber.css";
 
 function Sideber() {
   return (
-    <div>Sideber</div>
-  )
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>note</h1>
+        <button>add</button>
+      </div>
+        <div className="app-sidebar-notes">
+          <div className="app-sidebar-note">
+            <div className="sidebar-note-title">
+              <strong>title</strong>
+              </div>
+              <button>delete</button>
+              <p>content of this note</p>
+              <small>The last edited date : xx/xx</small>
+            </div>
+          
+        </div>
+      
+    </div>
+  );
 }
 
-export default Sideber
-// Path: src/components/Sideber.css 
+export default Sideber;
