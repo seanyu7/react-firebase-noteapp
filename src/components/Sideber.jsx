208ee0b4a94a085e6ec8e9b02d2142ec717604e8
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-11-07 23:59:12 +0900

    ⚡️ made some responsive app with using usestate

--- src/components/Sideber.jsx
+++ src/components/Sideber.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import "./Sideber.css";
 
-function Sideber() {
+function Sideber({ onAddNote, notes, deleteNote, activeNote, setActiveNote }) {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>note</h1>
-        <button>add</button>
+        <button onClick={onAddNote}>add</button>
       </div>
-        <div className="app-sidebar-notes">
-          <div className="app-sidebar-note">
+      <div className="app-sidebar-notes">
+        {notes.map((note) => (
+          <div
+            className={`app-sidebar-note ${note.id === activeNote && "active"}`}
+            key={note.id}
+            onClick={() => setActiveNote(note.id)}
+          >
             <div className="sidebar-note-title">
-              <strong>title</strong>
-              </div>
-              <button>delete</button>
-              <p>content of this note</p>
-              <small>The last edited date : xx/xx</small>
+              <strong>{note.title}</strong>
             </div>
-          
-        </div>
-      
+            <button onClick={() => deleteNote(note.id)}>delete</button>
+            <p>{note.content}</p>
+            <small>The last edited date : xx/xx</small>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
