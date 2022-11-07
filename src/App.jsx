e7064d1181f7530e4da58c182b329ab08300487b
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-11-07 23:57:05 +0900

    ⚡️ used a usestate to memorize a state of this app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 import "./App.css";
 import Main from "./components/Main";
 import Sideber from "./components/Sideber";
+import { useState } from "react";
+import uuid from "react-uuid";
 
 function App() {
-  return <div className="App">
-    <Sideber />
-    <Main />
-  </div>;
+  const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState(false);
+
+  const onAddNote = () => {
+    console.log("add note");
+    const newNote = {
+      id: uuid(),
+      title: "New Note",
+      content:
+        "learning react from the beginning and building a app from scratch",
+      lastModified: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const deleteNote = (id) => {
+    console.log("delete note");
+    setNotes(notes.filter((note) => note.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <Sideber
+        onAddNote={onAddNote}
+        notes={notes}
+        deleteNote={deleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
