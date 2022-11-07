94f34d5793d5bbe3157c1115d6ae4a2c7d3327c1
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-11-08 01:23:41 +0900

    ⚡️ made a mark up first to make this main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./Main.css";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" placeholder="Note Title" />
+        <textarea id="" placeholder="put in content of the note" />
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">Title</h1>
+        <div className="markdown-preview">content of the note</div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
