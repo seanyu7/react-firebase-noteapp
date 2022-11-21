bc7ae6a4102f864f03d093788e8679d02b70ade7
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-11-22 00:04:12 +0900

    ⚡️ made a function to line in a order in this app

--- src/components/Sideber.jsx
+++ src/components/Sideber.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Sideber.css";
 
-function Sideber({ onAddNote, notes, deleteNote, activeNote, setActiveNote }) {
+const Sideber = ({
+  onAddNote,
+  notes,
+  deleteNote,
+  activeNote,
+  setActiveNote,
+}) => {
+
+const sortedNotes = notes.sort((a, b) => b.modDate - a.modDate);
+//この書き方をすることによって、今まで書いてきたノートを修正した際に過去のノートよりも上に上がってくるため、
+//どのノートが最新のものなのかを簡単に判断できるようになる。
+
+
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
@@ -9,7 +21,7 @@ function Sideber({ onAddNote, notes, deleteNote, activeNote, setActiveNote }) {
         <button onClick={onAddNote}>add</button>
       </div>
       <div className="app-sidebar-notes">
-        {notes.map((note) => (
+        {sortedNotes.map((note) => (
           <div
             className={`app-sidebar-note ${note.id === activeNote && "active"}`}
             key={note.id}
@@ -20,15 +32,18 @@ function Sideber({ onAddNote, notes, deleteNote, activeNote, setActiveNote }) {
             </div>
             <button onClick={() => deleteNote(note.id)}>delete</button>
             <p>{note.content}</p>
-            <small>The last edited date :{new Date(note.modDate).toLocaleDateString("jp",{
-              hour: "2-digit",
-              minute: "2-digit",
-            })}</small>
+            <small>
+              The last edited date :
+              {new Date(note.modDate).toLocaleDateString("jp", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default Sideber;
